ac06858992f5fccef1af31b6f7baa6e6984b2379
twzkraus <twzkraus@gmail.com>
2020-09-08 17:05:59 -0700

    one fix added to update

--- datastore/index.js
+++ datastore/index.js
@@ -66,26 +66,36 @@ exports.update = (id, text, callback) => {
   fs.readdir(path.join(thisPath, '..'), (err, items) => {
     if (items.indexOf(`${id}.txt`) < 0) {
       callback(new Error(`No item with id: ${id}`));
+    } else {
+      fs.writeFile(thisPath, text, (err) => {
+        if (err) {
+          callback(new Error(`No item with id: ${id}`));
+        } else {
+          callback(null, { id, text });
+        }
+      });
     }
-    fs.writeFile(thisPath, text, (err) => {
-      if (err) {
-        callback(new Error(`No item with id: ${id}`));
-      } else {
-        callback(null, { id, text });
-      }
-    });
   });
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
+  let thisPath = path.join(__dirname, '..', 'test', 'testData', `${id}.txt`);
+  fs.readdir(path.join(thisPath, '..'), (err, items) => {
+    if (items.indexOf(`${id}.txt`) < 0) {
+      callback(new Error(`No item with id: ${id}`));
+    }
+    items.splice(id, 1);
     callback();
-  }
+  });
+
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
